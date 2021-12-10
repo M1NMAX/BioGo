18e69fbf6ab13a1771f7322914cf854e28348175
M1NMAX <carlosaurora20@gmail.com>
2021-12-10 08:57:55 +0000

    show player ranking

--- app/src/main/java/dev/biogo/RankingActivity.java
+++ app/src/main/java/dev/biogo/RankingActivity.java
@@ -1,14 +1,61 @@
 package dev.biogo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class RankingActivity extends AppCompatActivity {
+    private static final String TAG = "RankingActivity";
+    private DatabaseReference mDatabase;
+    ListView rankingListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
+        mDatabase = FirebaseDatabase.getInstance("https://biogo-54daa-default-rtdb.europe-west1.firebasedatabase.app/")
+                .getReference();
+
+        rankingListView = findViewById(R.id.rankingListView);
+        ArrayList<String> list = new ArrayList<>();
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, R.layout.ranking_list_item, list);
+        rankingListView.setAdapter(adapter);
+
+
+
+        DatabaseReference usersRef = mDatabase.child("users");
+        usersRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                list.clear();
+                for (DataSnapshot snapshot: dataSnapshot.getChildren() ){
+                    list.add(snapshot.child("username").getValue().toString());
+                    Log.i(TAG, "onDataChange: "+snapshot.child("username").getValue());
+                }
+                Log.d(TAG, "onDataChange: "+list.size());
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w(TAG, "loadPost:onCancelled", error.toException());
+            }
+        });
+
     }
 }

0d4696801471ae49c6df2d5b75cfa16a8114dbdb
M1NMAX <carlosaurora20@gmail.com>
2021-12-11 20:43:56 +0000

    startActivityForResult refactor

--- app/src/main/java/dev/biogo/HomeFragment.java
+++ app/src/main/java/dev/biogo/HomeFragment.java
@@ -1,73 +1,45 @@
 package dev.biogo;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.ProgressDialog;
-import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class HomeFragment extends Fragment implements View.OnClickListener {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     private static final String TAG = "HomeFragment";
     private boolean isOpen = true;
@@ -75,13 +47,9 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
 
     }
-    private static final int IMAGE_REQUEST_CODE = 2;
     FloatingActionButton fabAddPhoto;
     FloatingActionButton fabUploadPhoto;
     private Uri imageUri;
@@ -130,6 +98,7 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         return view;
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -157,32 +126,33 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
                 break;
             case R.id.fabUploadPhoto:
                 openImage();
-                Toast.makeText(getContext(), "goog", Toast.LENGTH_LONG).show();
                 break;
         }
 
 
     }
+    ActivityResultLauncher<Intent> imageActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == Activity.RESULT_OK){
+                        Intent data = result.getData();
+                        if (data!=  null) {
+                            imageUri = data.getData();
+                            uploadImage();
+                        }
+                    }
+                }
+            }
+    );
 
     private void openImage() {
         Intent imageIntent = new Intent();
         imageIntent.setType("image/");
         imageIntent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(imageIntent, IMAGE_REQUEST_CODE);
+        imageActivityResultLauncher.launch(imageIntent);
     }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == IMAGE_REQUEST_CODE && resultCode == getActivity().RESULT_OK){
-            imageUri = data.getData();
-            uploadImage();
-
-        }
-
-    }
-
-
 
     private void uploadImage() {
         ProgressDialog pd= new ProgressDialog(getContext());
@@ -191,21 +161,14 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         if (imageUri != null){
             StorageReference fileRef = FirebaseStorage.getInstance().getReference()
                     .child("image/").child(String.valueOf(System.currentTimeMillis()));
-            fileRef.putFile(imageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
-                    fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                        @Override
-                        public void onSuccess(@NonNull Uri uri) {
-                            String url = uri.toString();
-                            Log.d(TAG, "onSuccess: "+url);
-                            pd.dismiss();
-                            Toast.makeText(getContext(), "Success", Toast.LENGTH_LONG).show();
+            fileRef.putFile(imageUri).addOnCompleteListener(task ->
+                    fileRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                String url = uri.toString();
+                Log.d(TAG, "onSuccess: "+url);
+                pd.dismiss();
+                Toast.makeText(getContext(), "Success", Toast.LENGTH_LONG).show();
 
-                        }
-                    });
-                }
-            });
+            }));
         }
 
     }

52b168c0aa1065506cc02b0b8b064fe061fbac25
M1NMAX <carlosaurora20@gmail.com>
2022-01-18 13:32:47 +0000

    upd: specie ui

--- app/src/main/java/dev/biogo/Enums/ClassificationEnum.java
+++ app/src/main/java/dev/biogo/Enums/ClassificationEnum.java
@@ -1,18 +1,7 @@
 package dev.biogo.Enums;
 
 public enum ClassificationEnum {
-        VALID(0),
-        PENDING(0),
-        INVALID(-1);
-
-
-        private int xp;
-        private float color;
-        ClassificationEnum(int xp){
-                this.xp = xp;
-        }
-        ClassificationEnum(float color) {this.color = color;}
-        public int getValue(){return this.xp;}
-
-
+    VALID(),
+    PENDING(),
+    INVALID();
 }
